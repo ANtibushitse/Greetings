091d3825b28136d0a08fa565a818a18b64b03722
ANtibushitse <ANtibushitse@users.noreply.github.com>
2016-10-07 09:38:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,20 +8,13 @@ var lastname = $(inputs[1]).val();
 console.log(lastname);
 var city = $(inputs[2]).val();
 console.log(city);
-// etape deux je créer un objet 
-/*var obj = { propriété_1:   valeur_1,   // propriété_# peut être un identifiant
-            2:             valeur_2,   // ou un nombre
-            // ...,
-            "propriété n": valeur_n }; // ou une chaîne*/
+
 var foo = {
 	prenom: $("#first_name").val()
 	, nom: $("#last_name").val()
 	, ville: $("#city").val()
 , };
 console.log(foo);
-// Etape trois cconcaténer firstname+ last name. 
+
+
 $("#username").text(foo.prenom + foo.nom);
-// THE END .
-//"Doo or doo Not ther is no try" un petit être vert.
-//"Doo or doo Not ther is no try" un petit être vert.
-//"Doo or doo Not ther is no try" un petit être vert.
